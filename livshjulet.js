9a6ba83e920450f0bcde7ed4d39b0a5de0d2db0c
bjafl-sps <bjarte@sanpro.no>
2024-11-07 12:32:07 +0100

    interactive chart. Hover+click.

--- livshjulet.js
+++ livshjulet.js
@@ -25,9 +25,11 @@ root.setThemes([
   am5themes_Animated.new(root),
   am5themes_Material.new(root)
 ]);
+root.numberFormatter.set("numberFormat", "#.");
 let chart = root.container.children.push(am5radar.RadarChart.new(root, {
   panX: false,
   panY: false
+  //cursor: am5xy.XYCursor.new(root, {})
 }));
 let colorSet = am5.ColorSet.new(root, {
   colors: [
@@ -50,176 +52,136 @@ let xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
   maxDeviation: 0,
   categoryField: "category",
   renderer: xRenderer,
-  tooltip: am5.Tooltip.new(root, {})
+  tooltip: am5.Tooltip.new(root, {
+    forceHidden: true
+  })
 }));
+let yRenderer = am5radar.AxisRendererRadial.new(root, {
+  minGridDistance: 20
+});
 let yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
   min: 0,
   max: 10,
-  renderer: am5radar.AxisRendererRadial.new(root, {
-    minGridDistance: 20
-  })
+  renderer: yRenderer
 }));
 yAxis.get("renderer").labels.template.set("forceHidden", true);
+
 
 // Create series
 let series = chart.series.push(am5radar.RadarColumnSeries.new(root, {
   xAxis: xAxis,
   yAxis: yAxis,
   valueYField: "value",
-  categoryXField: "category"
+  categoryXField: "category",
+  opacity: 0.8
 }));
-
+let seriesHover = chart.series.push(am5radar.RadarColumnSeries.new(root, {
+  xAxis: xAxis,
+  yAxis: yAxis,
+  valueYField: "value",
+  categoryXField: "category",
+  opacity: 0.5
+}));
 series.columns.template.setAll({
   tooltipText: "{categoryX}: {valueY}",
+  //showTooltipOn: "click",
   templateField: "columnSettings",
   strokeOpacity: 0,
   width: am5.p100
 });
+seriesHover.columns.template.setAll({
+  templateField: "columnSettings",
+  strokeOpacity: 0,
+  width: am5.p100
+});
+
 let data = [{
   category: kat1Tittel,
-  value: 0,
-  columnSettings: {
-    fill: colorSet.next()
-  }
 }, {
   category: kat2Tittel,
-  value: 0,
-  columnSettings: {
-  fill: colorSet.next()
-            }
 }, {
   category: kat3Tittel,
-  value: 0,
-  columnSettings: {
-    fill: colorSet.next()
-  }
 }, {
   category: kat4Tittel,
-  value: 0,
-  columnSettings: {
-    fill: colorSet.next()
-  }
 }, {
   category: kat5Tittel,
-  value: 0,
-  columnSettings: {
-    fill: colorSet.next()
-  }
 }, {
   category: kat6Tittel,
-  value: 0,
-  columnSettings: {
-    fill: colorSet.next()
-  }
 }, {
   category: kat7Tittel,
-  value: 0,
-  columnSettings: {
-    fill: colorSet.next()
-  }
 }, {
   category: kat8Tittel,
-  value: 0,
-  columnSettings: {
-    fill: colorSet.next()
-  }
 }];
+let i = 0;
+data.forEach((d)=>{
+  d['value'] = 0;
+  d['columnSettings'] = {fill: colorSet.next()};
+  d['index'] = i;
+  i++;
+});
+dataHover = [...data];
+data.forEach((d)=>{
+  d.columnSettings['opacity'] = 0.8;
+});
 series.data.setAll(data);
+seriesHover.data.setAll(dataHover);
 xAxis.data.setAll(data);
+series.set("clustered",  false);
+seriesHover.set("clustered",  false);
 
 // Animate chart
 series.appear(1000);
 chart.appear(1000, 100);
 
-// Add click event to change value
-series.columns.template.events.on("click", function (ev) {
-  // Get the current data item
-  let dataItem = ev.target.dataItem;
-  
-  console.log("clicked")
-  if (dataItem) {
-      // Increment value on click
-      let newValue = dataItem.get("valueY") + 10; // Adjust increment as needed
-      dataItem.set("valueY", newValue > 100 ? 0 : newValue); // Reset to 0 if it exceeds 100
-      console.log(dataItem)
-  }
-});
+// Hover event
+let lastHoverDataIndex = 0;
+let lastHoverValue = 0;
+let cursor = chart.set("cursor", am5radar.RadarCursor.new(root, {}));
+cursor.lineX.set("visible", false);
+cursor.lineY.set("visible", false);
+cursor.events.on("cursormoved", function(ev) {
+  const posX = ev.target.getPrivate("positionX");
+  const posY = ev.target.getPrivate("positionY");
+  const x = xAxis.toAxisPosition(posX);
+  const y = yAxis.toAxisPosition(posY);
+  const item = xAxis.getSeriesItem(seriesHover, x);
+  const dataIndex = item.dataContext.index;
+  const yMax = yAxis.get("max");
+  const oldVal = item.dataContext.value;
+  const newVal = Math.min(Math.ceil(y * yMax), yMax);
 
-// Add a click event on the entire chart
-chart.events.on("click", function (ev) {
-  // Get the coordinates of the click relative to the chart's origin
+  if (lastHoverDataIndex != dataIndex){
+    seriesHover.data.setIndex(lastHoverDataIndex, {...seriesHover.data.getIndex(lastHoverDataIndex), value: 0});
+    series.columns.getIndex(lastHoverDataIndex).set("opacity", 0.8);
+    series.columns.getIndex(dataIndex).set("opacity", 0.3);
+    lastHoverDataIndex = dataIndex;
+  }
+  if (oldVal == newVal)
+    return;
+  seriesHover.data.setIndex(dataIndex, {...seriesHover.data.getIndex(dataIndex), value: newVal});
+  lastHoverValue = newVal;
+
+  //console.log(dataIndex, ":", item.dataContext.category, "=", newVal);
+  });
+
+chart.events.on("globalpointermove", function (ev) {
+  let x = ev.point.x;
+  let y = ev.point.y;
   let chartCenterX = chart.width() / 2;
   let chartCenterY = chart.height() / 2;
-
-  let clickX = ev.point.x;
-  let clickY = ev.point.y;
-
-  // Calculate the distance from the center
   let distance = Math.sqrt(
-      Math.pow(clickX - chartCenterX, 2) +
-      Math.pow(clickY - chartCenterY, 2)
+      Math.pow(x - chartCenterX, 2) +
+      Math.pow(y - chartCenterY, 2)
   );
   let maxRadius = yAxis.maxHeight();
 
-  if (distance > maxRadius * 1.1)
-    return;
-
-  // Calculate the new value based on the distance
-  const maxVal = yAxis.get("max");
-  let newValue = Math.ceil(Math.min(maxVal, (distance / maxRadius) * maxVal));
-
-  // Determine which sector (or category) was clicked based on angle
-  let angle = Math.atan2(clickY - chartCenterY, clickX - chartCenterX) 
-      * (180 / Math.PI) + 90; // +90 to adjust for different 0-point
-  angle = (angle + 360) % 360; // Normalize angle to [0, 360)
-
-  // Identify which category was clicked based on angle
-  let clickedCategory = null;
-  let sectorAngleSize = 360 / data.length;
-  data.forEach((item, index) => {
-      if (angle >= sectorAngleSize * index && angle < sectorAngleSize * (index + 1)) {
-          clickedCategory = item.category;
-
-          // Update the data value for the clicked category
-          data[index].value = newValue;
-
-          // Log the information
-          console.log("angle: ", angle);
-          console.log("index: ", index);
-          console.log(`Clicked category: ${clickedCategory}`);
-          console.log(`New value: ${newValue.toFixed(2)}`);
-          console.log(`Distance from origin: ${distance.toFixed(2)} pixels`);
-      }
-  });
-
-  console.log(ev);
-  // Update the chart data to reflect the changes
-  series.data.setAll(data);
+  if (distance > maxRadius * 1.1){
+      seriesHover.data.setIndex(lastHoverDataIndex, {...seriesHover.data.getIndex(lastHoverDataIndex), value: 0});
+      series.columns.getIndex(lastHoverDataIndex).set("opacity", 0.8);
+  }
 });
 
-// Function for updating value
-function setValue(index, value) {
-  
-  // Set value
-  let row = data[index];
-  row.value = value;
-  console.log(row.columnSettings);
-  series.data.setIndex(index, {
-    category: row.category,
-    value: value,
-    columnSettings: row.columnSettings
-  });
-  
-  // Marker valgt tall
-  let areas = document.getElementsByClassName("area");
-  let valueBtns = areas[index].getElementsByClassName("value");
-  console.log(valueBtns.length);
-  for (let i = 0; i < valueBtns.length; i++) {
-    if (i == value-1) {
-      valueBtns[i].classList.add("clicked");
-    } else {
-      valueBtns[i].classList.remove("clicked");
-    }
-  }
-  //valueBtns[value-1].classList.add("clicked");
-}
+// Click event
+chart.events.on("click", function (ev) {
+  series.data.setIndex(lastHoverDataIndex, {...series.data.getIndex(lastHoverDataIndex), value: lastHoverValue});
+});
